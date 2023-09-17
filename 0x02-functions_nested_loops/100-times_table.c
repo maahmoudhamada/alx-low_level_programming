b6f2d4c77c14b3f2c9b8f5f921c5f0831c99bdfb
maahmoudhamada <maahmoud.hamada@hotmail.com>
2023-09-17 03:27:15 +0300

    100-times_table.c "Edited"

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,46 +9,34 @@
 
 void print_times_table(int n)
 {
-int i, j, mult;
+int i, j, res;
 
-i = j = mult = 0;
-
-if ((n > 15) || (n < 0))
-_putchar('\n');
-else
+i = j = res = 0;
+if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
-for (j = 0; j <= n; j++)
-{
-mult = i *j;
-if (j != 0)
+_putchar('0');
+for (j = 1; j <= n; j++)
 {
 _putchar(',');
 _putchar(' ');
-}
-if ((mult < 10) && (mult >= 0) && j != 0)
-{
+
+res = i *j;
+if (res <= 9)
 _putchar(' ');
+if (res <= 99)
 _putchar(' ');
-_putchar((mult % 10) + '0');
-}
-else if (mult >= 10 && mult < 100)
+
+if (res >= 10 && res < 100)
+_putchar(res / 10 + '0');
+
+else if (res >= 100 && res < 1000)
 {
-_putchar(' ');
-_putchar((mult / 10) + '0');
-_putchar((mult % 10) + '0');
+_putchar((res / 10) / 10 + '0');
+_putchar((res / 10) % 10 + '0');
 }
-else if (mult >= 100 && mult < 1000)
-{
-_putchar((mult / 10) / 10 + '0');
-_putchar((mult / 10) % 10 + '0');
-_putchar((mult % 10) + '0');
-}
-else
-{
-_putchar(mult % 10 + '0');
-}
+_putchar(res % 10 + '0');
 }
 _putchar('\n');
 }
