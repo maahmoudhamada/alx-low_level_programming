fb2be039e43aec3d858dd3de31a4a67701b74660
maahmoudhamada <maahmoud.hamada@hotmail.com>
2023-10-01 07:02:43 +0300

    0x09-static_libraries Tasks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,31 +1,21 @@
-/**
- * print_number - Function to print an integer
- *
- * @n: Number to be printed
- *
- * Return: Void
-*/
 #include "main.h"
 
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n *= -1;
-}
-if (n >= 10 && n < 100)
-_putchar(n / 10 + '0');
-else if (n >= 100 && n < 1000)
-{
-_putchar(((n / 10) / 10) + '0');
-_putchar(((n / 10) % 10) + '0');
-}
-else if (n >= 1000)
-{
-_putchar(((n / 100) / 10) + '0');
-_putchar(((n / 100) % 10) + '0');
-_putchar(((n / 10) % 10) + '0');
-}
-_putchar((n % 10) + '0');
+    unsigned int num = n;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        num = -num;
+    }
+
+    if ((num / 10) > 0)
+        print_number(num / 10);
+
+    _putchar((num % 10) + '0');
 }
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,7 @@
+#0x09-Static_Libraries
+
+**1. libmy.a:** Create Static library containing functions
+
+ **2. main.h:** Prototypes of functions used in libmy.a library
+
+ **3. create_static_lib.sh:** Script that creats a static library from all .c files in the current directory
