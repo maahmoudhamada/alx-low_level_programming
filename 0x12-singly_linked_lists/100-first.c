c5e30d13e4daa4bdee58347ffcf734cf0c16ef86
maahmoudhamada <maahmoud.hamada@hotmail.com>
2023-11-16 19:34:35 +0200

    0x12-singly_linked_lists -> 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+
+/**
+* pre_execute - Function that prints before main execute
+*
+* Return: Void
+*/
+void pre_execute(void)
+{
+printf("You're beat! and yet, you must allow,
+\nI bore my house upon my back!\n");
+
+}
+
