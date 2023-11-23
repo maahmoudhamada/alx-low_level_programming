b4f954a852550493ad21972051c11267b6aae13e
maahmoudhamada <maahmoud.hamada@hotmail.com>
2023-11-23 22:14:56 +0200

    0x14-bit -> last mand. & Adv.

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,53 +1,27 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* bit_len - Function that gets number length using bit manu
+* flip_bits - Function that flips bits and return needed flips
 *
-* @n: Number to be measured
+* @n: First number
+* @m: Second number
 *
-* Return: Length (integer);
+* Return: Number of flips needed
 */
-
-int bit_len(int n)
-{
-int len = 0;
-while (n > 0)
-{
-len++;
-n = n >> 1;
-}
-return (len);
-}
-
-/**
-* flip_bits - Function returns number needed to flip to get from one to another
-*
-* @n: First Number (from)
-* @m: Second Number (to)
-*
-* Return: Number of flips
-*/
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned long int len1, len2, lenf, i;
-unsigned int ret;
+unsigned long int res;
+unsigned int count = 0;
+unsigned long int i;
 
-len1 = len2 = lenf = i = ret = 0;
+res = n ^ m;
 
-len1 = bit_len(n);
-len2 = bit_len(m);
-lenf = (len1 > len2) ? (len1) : len2;
-
-
-while (i < lenf)
+for (i = 0; i < sizeof(unsigned long int) * 8; i++)
 {
-ret += (n & 1) ^ (m & 1);
-i++;
-n = n >> 1;
-m = m >> 1;
+if ((res >> i) & 1)
+count++;
 }
 
-return (ret);
+return (count);
 }
 
