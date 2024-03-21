89e6d7fb580accc4e4b35fc6bc596589e160e49c
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-03-21 20:36:11 +0200

    0x017 -> 5-get_dnodeint.c "Fix - adding function"

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+* dlistint_len - Function to get length of doubly linked list
+*
+* @h: Head of doubly linked list
+*
+* Return: Length of list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+const dlistint_t *ptr;
+size_t len = 0;
+
+for (len = 0, ptr = h; ptr; ptr = ptr->next, len++)
+;
+return (len);
+}
+
+/**
 * get_dnodeint_at_index - Function to get a specific node
 *
 * @head: Head of doubly linked list
--- 0x17-doubly_linked_lists/suite/5-main.c
+++ 0x17-doubly_linked_lists/suite/5-main.c
@@ -24,6 +24,7 @@ int main(void)
     add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     node = get_dnodeint_at_index(head, 5);
+    printf("%d\n", node->n);
     free_dlistint(head);
     head = NULL;
     return (EXIT_SUCCESS);
