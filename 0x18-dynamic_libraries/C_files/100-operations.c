9d127f6f878832f9a6088818a93078d20498df3b
maahmoudhamada <maahmoud.hamada@hotmail.com>
2024-03-26 00:56:37 +0200

    0x18 -> 100-operations.so

---
+++ 0x18-dynamic_libraries/C_files/100-operations.c
@@ -0,0 +1,72 @@
+#include "operations.h"
+
+/**
+* add - Function adds a and b
+*
+* @a: First num
+* @b: Second num
+*
+* Return: Sum of two numbers
+*/
+
+int add(int a, int b)
+{
+return ((a) + (b));
+}
+
+/**
+* sub - Function sub a and b
+*
+* @a: First num
+* @b: Second num
+*
+* Return: Sub of two numbers
+*/
+
+int sub(int a, int b)
+{
+return ((a) - (b));
+}
+
+/**
+* mul - Function mul a and b
+*
+* @a: First num
+* @b: Second num
+*
+* Return: Mul of two numbers
+*/
+
+int mul(int a, int b)
+{
+return ((a) * (b));
+}
+
+/**
+* div - Function div a and b
+*
+* @a: First num
+* @b: Second num
+*
+* Return: Div of two numbers
+*/
+
+int div(int a, int b)
+{
+return ((a) / (b));
+}
+
+/**
+* mod - Function mod a and b
+*
+* @a: First num
+* @b: Second num
+*
+* Return: mod of two numbers
+*/
+
+int mod(int a, int b)
+{
+return ((a) % (b));
+}
+
